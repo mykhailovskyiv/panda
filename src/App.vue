ff76acc3a971cb8bfa92948922546788d48d59d7
mykhailovskyiv <valeriimykhailovskyi@gmail.com>
2023-02-11 13:27:49 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <weather></weather>
+  </div>
+</template>
+
+<script>
+
+import Weather from "@/components/Weather";
+export default {
+  name: 'App',
+  components: {
+    Weather,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+  box-sizing: border-box;
+  padding: 0;
+}
+h1,h2,h3,h4,p,span {
+  margin: 0;
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <Logo></Logo>
+    <h1>Panda Weather</h1>
+    <div class="select-city">
+      <input autocomplete="true" v-model="city" @input="findCity">
+      <ul>
+        <li v-for="item in filteredCity" :key="item.index">{{item.city}}</li>
+      </ul>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "../assets/Logo"
+import cities from "../assets/ua.json"
+
+export default {
+  name: "Weather",
+  data() {
+    return {
+      city: '',
+      cities: cities,
+      filteredCity: []
+    }
+  },
+  components: {
+    Logo
+  },
+  methods: {
+    findCity() {
+      if (this.city.length !==0) {
+        this.filteredCity = this.cities.filter(item => {
+          return item.city.toLowerCase().startsWith(this.city.toLowerCase())
+        })
+      } else {
+        this.filteredCity =[]
+      }
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
