015c69799a463c3d1d4e2d5a371db7939ae9c9f3
mykhailovskyiv <valeriimykhailovskyi@gmail.com>
2023-02-11 14:56:44 +0200

    test

---
+++ src/components/CityCard.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class="card" v-if="item">
+   <h5>{{ item.name }}</h5>
+   <p>{{ getItemWeather().main }}</p>
+   <p>{{ getItemWeather().description }}</p>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "CityCard",
+  props: {
+    item :{
+      type: Object
+    }
+  },
+  methods: {
+    getItemWeather() {
+      return this.item.weather[0]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="container">
     <Logo></Logo>
-    <h1>Panda Weather v1</h1>
+    <h1>Panda Weather</h1>
     <div class="select-city">
       <input autocomplete="true" v-model="city" @input="findCity">
       <ul>
-        <li v-for="item in filteredCity" :key="item.index">{{item.city}}</li>
+        <li v-for="item in filteredCity" :key="item.index" @click="setCity(item.city)">{{item.city}}</li>
       </ul>
-
+    </div>
+    <div class="card-list">
+      <city-card :item="cityWeather"></city-card>
     </div>
   </div>
 </template>
@@ -15,6 +17,7 @@
 <script>
 import Logo from "../assets/Logo"
 import cities from "../assets/ua.json"
+import CityCard from "@/components/CityCard";
 
 export default {
   name: "Weather",
@@ -22,11 +25,13 @@ export default {
     return {
       city: '',
       cities: cities,
-      filteredCity: []
+      filteredCity: [],
+      cityWeather: null
     }
   },
   components: {
-    Logo
+    Logo,
+    CityCard
   },
   methods: {
     findCity() {
@@ -37,6 +42,19 @@ export default {
       } else {
         this.filteredCity =[]
       }
+    },
+    setCity(city) {
+      this.city = city
+      this.getWeather()
+      this.filteredCity =[]
+    },
+    getWeather() {
+      console.log(this.city)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=a037c87fc70dc90f55aeda70b13e314c`)
+      .then(resp => resp.json())
+      .then( resp => {
+        this.cityWeather = resp
+      })
     }
   }
 
