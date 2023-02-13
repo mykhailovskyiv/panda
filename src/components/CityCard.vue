ea492f8588a133aee3cc5614a8617ed2b19e0661
mykhailovskyiv <valeriimykhailovskyi@gmail.com>
2023-02-14 00:29:46 +0200

    chart

--- src/components/CityCard.vue
+++ src/components/CityCard.vue
@@ -2,8 +2,14 @@
  <div class="card" v-if="item">
    <div class="card__container">
      <h5 class="card__title">{{ item.city.name }}</h5>
-     <p class="card__description">Temp {{ temperatureOnCelsius(item.list[0].main.temp) }}</p>
-     <p class="card__description">Feels like {{ temperatureOnCelsius(item.list[0].main.feels_like) }}</p>
+     <img v-if="getItemWeather().main === 'Clear sky' " src="http://openweathermap.org/img/wn/01d@2x.png" alt="clear sky">
+     <img v-if="getItemWeather().main === 'Clouds' " src="http://openweathermap.org/img/wn/03d@2x.png" alt="Clouds">
+     <img v-if="getItemWeather().main === 'Rain' " src="http://openweathermap.org/img/wn/10d@2x.png" alt="Rain">
+     <img v-if="getItemWeather().main === 'Thunderstorm' " src="http://openweathermap.org/img/wn/11d@2x.png" alt="Thunderstorm">
+     <img v-if="getItemWeather().main === 'Snow' " src="http://openweathermap.org/img/wn/13d@2x.png" alt="Snow">
+
+     <p class="card__description">Temp {{ temperatureOnCelsius(item.list[0].main.temp) }}°C</p>
+     <p class="card__description">Feels like {{ temperatureOnCelsius(item.list[0].main.feels_like) }}°C</p>
      <p class="card__description">{{ getItemWeather().main }}</p>
      <p class="card__description">{{ getItemWeather().description }}</p>
      <button @click="addToFavorite" class="card__button">Add to favorite</button>
@@ -51,7 +57,7 @@ export default {
   border-radius: 35px;
   background-color: black;
   &__container {
-    background: aliceblue;
+    background: #f5fbff;
     border-radius: 30px;
     padding-bottom: 10px;
   }
